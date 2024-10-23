900052fc4aa07cee8f9427cbe1563f34ee87f531
Ashri024 <ashriarya.024@gmail.com>
2024-10-23 12:52:38 +0530

    Created landing page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+        colors: {
+          primary: "#399B2E",
+          lineColor:"#128325",
+          footer: "#003404",
+        },
+      },
+  },
+  plugins: [],
+}
+
+
